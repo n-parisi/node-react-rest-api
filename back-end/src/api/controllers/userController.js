904f5639c06a18c2199e08c4ec9eb9bd4fb13f31
Nick Parisi <nparisi2392@gmail.com>
2019-01-05 22:46:17 -0500

    Add JSON web token authentication. Add .gitignore file.

--- back-end/src/api/controllers/userController.js
+++ back-end/src/api/controllers/userController.js
@@ -6,6 +6,8 @@
 "use strict";
 
 var mongoose = require("mongoose");
+var jwt = require("jsonwebtoken");
+var config = require("../../config");
 var User = mongoose.model("User");
 
 /**
@@ -17,7 +19,7 @@ var User = mongoose.model("User");
 function getAllUsers(req, res) {
   User.find({}, function(err, user) {
     if (err) {
-      res.send(err);
+      return res.send(err);
     }
     res.json(user);
   });
@@ -25,6 +27,7 @@ function getAllUsers(req, res) {
 
 /**
  * Create a user from request JSON body. Return ID of newly created user as response.
+ * Also returns JSON web token for authenticating update/delete operations on this user.
  *
  * @param {Request} req
  * @param {Response} res
@@ -33,10 +36,15 @@ function createUser(req, res) {
   var newUser = new User(req.body);
   newUser.save(function(err, user) {
     if (err) {
-      res.send(err);
-    } else {
-      res.json(user._id);
+      return res.send(err);
     }
+    //if succesfully registered this user, create JSON web token for future modifications
+    var token = jwt.sign({ id: user._id }, config.tokenkey);
+
+    res.json({
+      userId: user._id,
+      authToken: token
+    });
   });
 }
 
@@ -49,40 +57,44 @@ function createUser(req, res) {
 function getUser(req, res) {
   User.findById(req.params.userId, function(err, user) {
     if (err) {
-      res.send(err);
+      return res.send(err);
     }
     res.json(user);
   });
 }
 
 /**
- * Update a user, with userId parameter encoded in URL, with JSON request body. Return updated user as JSON response.
+ * Update a user, with userId parameter encoded in URL, by replacing it with user defined in JSON request body. Return updated user as JSON response.
  *
  * @param {Request} req
  * @param {Response} res
  */
 function updateUser(req, res) {
-  //check to see if subscription level to be updated to is a valid subscription level (it is in User.subscription enum),
-  //respond with error message if it is not.
-  if (req.body.subscription) {
-    var subscriptionEnum = User.schema.path("subscription").enumValues;
-    if (!Object.values(subscriptionEnum).includes(req.body.subscription)) {
-      res.json({ message: "Invalid subscription level!" });
-      return;
-    }
-  }
+  //verify that end user has permission to delete this user by checking JSON web token
+  var verified = verifyUser(req, res);
 
-  User.findOneAndUpdate(
-    { _id: req.params.userId },
-    req.body,
-    { new: true }, //so we return updated user instead of original user
-    function(err, user) {
-      if (err) {
-        res.send(err);
+  if (verified) {
+    //check to see if subscription level to be updated to is a valid subscription level (it is in User.subscription enum),
+    //respond with error message if it is not.
+    if (req.body.subscription) {
+      var subscriptionEnum = User.schema.path("subscription").enumValues;
+      if (!Object.values(subscriptionEnum).includes(req.body.subscription)) {
+        return res.json({ message: "Invalid subscription level!" });
       }
-      res.json(user);
     }
-  );
+
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      req.body,
+      { new: true }, //so we return updated user instead of original user
+      function(err, user) {
+        if (err) {
+          return res.send(err);
+        }
+        res.json(user);
+      }
+    );
+  }
 }
 
 /**
@@ -92,12 +104,49 @@ function updateUser(req, res) {
  * @param {Response} res
  */
 function deleteUser(req, res) {
-  User.remove({ _id: req.params.userId }, function(err, user) {
-    if (err) {
-      res.send(user);
-    }
-    res.json({ message: "User successfully deleted" });
-  });
+  //verify that end user has permission to delete this user by checking JSON web token
+  var verified = verifyUser(req, res);
+
+  if (verified) {
+    User.remove({ _id: req.params.userId }, function(err, user) {
+      if (err) {
+        return res.send(user);
+      }
+      res.json({ message: "User successfully deleted" });
+    });
+  }
+}
+
+/**
+ * Verify that the end user has permission to perform this action. This is done by decoding the JSON web token in the x-access-token header. Web token decodes into
+ * userId of user that was created when web token was issued. If decoded web token matches the userId requested to perform operations on, verification is succesful
+ *
+ * @param {*} req
+ * @param {*} res
+ * @returns boolean that represents verification status
+ */
+function verifyUser(req, res) {
+  var token = req.headers["x-access-token"];
+  if (!token) {
+    res.json({ message: "No authentication token provided!" });
+    return false;
+  } else {
+    //verify token, make sure it is the right token for modifying this user by checking if it is equivalent to user id
+    var verified = false;
+    jwt.verify(token, config.tokenkey, function(err, decodedToken) {
+      if (err) {
+        return res.json({ message: "Failed to authenticate token!" });
+      }
+
+      if (decodedToken.id === req.params.userId) {
+        verified = true;
+      } else {
+        res.json({ message: "Authentication token does not match uesr!" });
+      }
+    });
+
+    return verified;
+  }
 }
 
 //make controller functions accessible to importing module
---
+++ back-end/src/config.js
@@ -0,0 +1,8 @@
+/**
+ * Define a token key which is used to encode/decode JSON web tokens along with userId.
+ * This key can be anything but should not be known to users. Please change this key before deploying.
+ */
+
+module.exports = {
+  tokenkey: "secretpassword"
+};
